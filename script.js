04ce803c8ef31683a11bcd4f55971fb5cb6b3155
RaphaelVerdaguer <raphaelverdaguer@me.com>
2024-04-19 23:42:56 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,110 @@
+// Créer une nouvelle requête XMLHttpRequest
+const xhr = new XMLHttpRequest();
+
+var categories;
+
+// Spécifier le type de requête et l'URL du fichier JSON
+xhr.open('GET', './categories.json', true);
+
+// Définir le type de réponse attendue
+xhr.responseType = 'json';
+
+// Gérer l'événement de chargement de la réponse
+xhr.onload = function() {
+  if (xhr.status === 200) {
+    // Les données JSON sont accessibles via xhr.response
+    categories = xhr.response;
+    // Utilisez les données JSON ici
+    console.log(categories);
+    drawCard();
+  } else {
+    console.error('Erreur lors du chargement du fichier JSON');
+  }
+};
+
+// Gérer les erreurs réseau
+xhr.onerror = function() {
+  console.error('Erreur réseau lors du chargement du fichier JSON');
+};
+
+// Envoyer la requête
+xhr.send();
+
+const categoriesTitle = {
+    yellow: "personne, lieu ou animal",
+    blue: "objet",
+    orange: "action",
+    green: "mot difficile",
+    red: "défi"
+  };
+
+
+function displayCard(card) {
+  for (const [color, value] of Object.entries(card)) {
+    const categoryContentElement = document.getElementById(color).getElementsByClassName('category-content')[0];
+    categoryContentElement.textContent = value.charAt(0).toUpperCase() + value.slice(1);
+  }
+}
+
+function blurCategoriesValue() {
+    // Floute les valeurs des catégories
+    const categoryContentElements = document.querySelectorAll('.category-content');
+    categoryContentElements.forEach(element => {
+      element.classList.add('blur');
+      element.classList.add('mask-text');
+    });
+}
+
+function unBlurCategoriesValue() {
+  const categoryContentElements = document.querySelectorAll('.category-content');
+  categoryContentElements.forEach(element => {
+      element.classList.remove('blur');
+      element.classList.remove('mask-text');
+  });
+}
+
+function seeCard() {
+  unBlurCategoriesValue();
+  setTimeout(blurCategoriesValue, 2000);
+}
+
+const drawButton = document.querySelector('.drawButton');
+const seeButton = document.querySelector('.seeButton');
+let timer;
+
+function drawCard() {
+    drawButton.disabled = true; // Désactive le bouton
+    startTimer(60); // Commence le timer avec une durée de 60 secondes
+
+    const card = {};
+
+    for (const [color, category] of Object.entries(categories)) {
+        const randomIndex = Math.floor(Math.random() * category.length);
+        card[color] = category[randomIndex];
+    }
+
+    displayCard(card);
+}
+
+function startTimer(duration) {
+    let timerValue = duration;
+    drawButton.textContent = timerValue + ' s'; // Affiche le temps restant dans le bouton
+
+    timer = setInterval(function() {
+        timerValue--;
+        drawButton.textContent = timerValue + ' s'; // Met à jour le temps restant dans le bouton
+
+        if (duration-timerValue == 2) {
+          blurCategoriesValue();
+        } 
+        if (timerValue <= 0) {
+            clearInterval(timer);
+            drawButton.disabled = false; // Réactive le bouton
+            drawButton.textContent = 'Tirer une carte'; // Affiche le texte initial dans le bouton
+            unBlurCategoriesValue();
+        }
+    }, 1000);
+}
+
+drawButton.addEventListener('click', drawCard);
+seeButton.addEventListener('click', seeCard);
