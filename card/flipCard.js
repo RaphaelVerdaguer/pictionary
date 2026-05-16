87244c11e5b921081eb9d6283b6d62dc8e5f23f7
Raphaël VERDAGUER <r.verdaguer@technoconcept.com>
2026-05-16 15:41:04 +0200

    feat: refactor card draw flow

--- card/flipCard.js
+++
@@ -1,8 +0,0 @@
-// Préparer les éléments DOM
-const card = document.querySelector(".flip-card-inner");
-// Basculer l'état "flippé" de la carte
-function flipCard() {
-    card.classList.toggle("is-flipped");
-  }
-
-card.addEventListener("click", flipCard);
